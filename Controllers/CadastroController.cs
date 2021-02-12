3d0eccbe495f7d219ce9fdd5248a4541cc0be932
Paulo Brandão <paulobrandaoofficial@gmail.com>
2021-02-12 13:49:58 -0200

    View em andamento

---
+++ Controllers/CadastroController.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace InstaDev.Controllers
+{
+    [Route("Cadastro")]
+    public class CadastroController : Controller
+    {
+        public IActionResult Index() 
+        {
+            return View();
+        }
+    }
+}
